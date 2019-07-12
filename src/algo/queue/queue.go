117000f02aa952f7bf7a0c2511986f595c25890c
ltinyho <ltinyho@gmail.com>
2019-07-12 11:20:40 +0800

    add:链表队列

--- src/algo/queue/queue.go
+++ src/algo/queue/queue.go
@@ -1,6 +1,8 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ArrayQueue struct {
 	data  []interface{}
@@ -14,18 +16,18 @@ func NewArrayQueue(maxLength int) *ArrayQueue {
 }
 
 func (q *ArrayQueue) Enqueue(v interface{}) bool {
-	// 队列已满
-	if q.head == 0 && q.tail == q.limit {
-		fmt.Println("队列已满")
-		return false
-	}
 	// 已经到队尾,搬移数据
 	if q.tail == q.limit {
+		// 队列已满
+		if q.head == 0 {
+			fmt.Println("队列已满")
+			return false
+		}
 		for i := q.head; i < q.tail; i++ {
 			q.data[i-q.head] = q.data[i]
 			q.data[i] = nil
 		}
-		q.tail = 0 + (q.tail - q.head)
+		q.tail -= q.head
 		q.head = 0
 	}
 	q.data[q.tail] = v
@@ -49,5 +51,54 @@ func (q *ArrayQueue) String() string {
 	for i := q.head; i < q.tail; i++ {
 		s += fmt.Sprintf("%v ", q.data[i])
 	}
+	return s
+}
+
+type QueueNode struct {
+	next *QueueNode
+	val  interface{}
+}
+
+func NewQueueNode(val interface{}) *QueueNode {
+	return &QueueNode{val: val}
+}
+
+type LinkedQueue struct {
+	head *QueueNode
+	tail *QueueNode
+}
+
+func NewLinkedQueue() *LinkedQueue {
+	return &LinkedQueue{nil, nil}
+}
+
+func (q *LinkedQueue) Enqueue(val interface{}) bool {
+	newNode := NewQueueNode(val)
+	if q.head == nil {
+		q.head = newNode
+		q.tail = newNode
+		return true
+	}
+	q.tail.next = newNode
+	q.tail = q.tail.next
+	return true
+}
+func (q *LinkedQueue) Dequeue() (val interface{}) {
+	if q.head == nil {
+		fmt.Println("队列为空")
+		return nil
+	}
+	temp := q.head
+	q.head = q.head.next
+	return temp.val
+}
+func (q *LinkedQueue) String() string {
+	s := ""
+	cur := q.head
+	for cur != nil && cur.next != nil {
+		s += fmt.Sprintf("%v => ", cur.val)
+		cur = cur.next
+	}
+	s += fmt.Sprintf("%v", cur.val)
 	return s
 }
--- src/algo/queue/queue_test.go
+++ src/algo/queue/queue_test.go
@@ -6,12 +6,29 @@ import (
 )
 
 func TestNewArrayQueue(t *testing.T) {
-	queue := NewArrayQueue(4)
+	queue := NewArrayQueue(10)
 	queue.Enqueue(1)
 	queue.Dequeue()
 	queue.Enqueue(1)
 	queue.Enqueue(2)
 	queue.Enqueue(3)
+	queue.Dequeue()
+	queue.Enqueue(5)
+	queue.Enqueue(6)
+	queue.Dequeue()
+	queue.Enqueue(7)
+	fmt.Println(queue)
+}
+
+func TestNewLinkedQueue(t *testing.T) {
+	queue := NewLinkedQueue()
+	queue.Enqueue(1)
+	queue.Dequeue()
+	queue.Enqueue(1)
+	queue.Enqueue(2)
+	queue.Enqueue(3)
+	queue.Dequeue()
+	queue.Dequeue()
 	queue.Dequeue()
 	queue.Enqueue(5)
 	queue.Enqueue(6)
