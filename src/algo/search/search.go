3e87663c2a61677813245bed898e45f4c5094023
ltinyho <ltinyho@gmail.com>
2019-07-17 07:35:48 +0800

    update:二分查找变体

--- src/algo/search/search.go
+++ src/algo/search/search.go
@@ -16,6 +16,86 @@ func BinarySearch(array []int, num int) bool {
 	return false
 }
 
+// 查找第一个等于 num的值
+func BinarySearchFirstEq(array []int, num int) int {
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] == num {
+			if mid == 0 || array[mid-1] != num {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else if array[mid] > num {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+// 查找最后一个等于 num的值
+func BinarySearchLastEq(array []int, num int) int {
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] == num {
+			if mid == len(array)-1 || array[mid+1] != num {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		} else if array[mid] > num {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+// 查找第一个大于等于给定值的元素
+func BinarySearchFirstGtEq(array []int, num int) int {
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] >= num {
+			if mid == 0 || array[mid-1] < num {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+// 查找第一个小于等于给定值的元素
+func BinarySearchLastLtEq(array []int, num int) int {
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] <= num {
+			if mid == len(array)-1 || array[mid+1] > num {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
 func BinarySearchRecursive(array []int, num int) bool {
 	return binarySearchRecursive(array, 0, len(array)-1, num)
 }
--- src/algo/search/search_test.go
+++ src/algo/search/search_test.go
@@ -6,11 +6,27 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
-	res := BinarySearch([]int{1}, 1)
-	fmt.Println(res)
+	ary1 := []int{0, 1, 2, 3, 3, 4, 5, 6, 7}
+	var res int
+	res = BinarySearchFirstEq(ary1, 3)
+	if res != 3 {
+		t.Fatal("第一个等于", res)
+	}
+	res = BinarySearchFirstGtEq(ary1, 4)
+	if res != 5 {
+		t.Fatal("第一个大于等于", res)
+	}
+	res = BinarySearchLastEq(ary1, 3)
+	if res != 4 {
+		t.Fatal("最后一个等于", res)
+	}
+	res = BinarySearchLastLtEq(ary1, 6)
+	if res != 7 {
+		t.Fatal("最后一个小于等于", res)
+	}
 }
 
 func TestBinarySearchRecursive(t *testing.T) {
-	res := BinarySearchRecursive([]int{1, 2, 3, 4, 5, 6}, 5)
+	res := BinarySearchRecursive([]int{1, 3, 4, 4, 5, 6}, 3)
 	fmt.Println(res)
 }
