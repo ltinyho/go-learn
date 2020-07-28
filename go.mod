ae732b122c4272210a89d078834527b2b6bbaf86
ltinyho <ltinyho@gmail.com>
2020-07-29 00:12:35 +0800

    udp

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/ianlancetaylor/demangle v0.0.0-20200414190113-039b1ae3a340 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/pborman/uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
 )
---
+++ src/network/udp.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	log = logrus.NewEntry(func() *logrus.Logger {
+		return &logrus.Logger{
+			Out:          os.Stdout,
+			Hooks:        nil,
+			Formatter:    &logrus.JSONFormatter{},
+			ReportCaller: true,
+			Level:        logrus.DebugLevel,
+			ExitFunc:     nil,
+		}
+	}())
+)
+
+func main() {
+	ch := make(chan int, 0)
+	go func() {
+		addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8888")
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		conn, err := net.ListenUDP("udp", addr)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		go func() {
+			for {
+				buf := make([]byte, 1024)
+				n, add, err := conn.ReadFrom(buf)
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+				fmt.Printf("data:%x\n", buf[:n])
+				u := binary.LittleEndian.Uint16(buf[:n])
+				u1 := binary.BigEndian.Uint16(buf[:n])
+				fmt.Printf("data:%x,%d\n", u,u)
+				fmt.Printf("datau1:%x,%d\n", u1,u1)
+				_, err = conn.WriteTo(buf[:n], add)
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+			}
+		}()
+		ch <- 1
+	}()
+	conn, err := net.Dial("udp", ":8888")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	data := make([]byte, 2)
+	binary.LittleEndian.PutUint16(data, 43981)
+	_, err = conn.Write(data)
+	if err != nil {
+		log.Error(err)
+	}
+	time.Sleep(time.Hour)
+}
